8edc356ea117a407203bf8f9553609ce64e20e33
mritunjay90 <mrityunjayshah981427@gmail.com>
2026-05-07 12:43:57 +0545

    error_handleing

---
+++ day2/errorhandle.mjs
@@ -0,0 +1,15 @@
+// try {
+//     const city = "city";
+//     console.log(city);
+// } catch (error) {
+//     console.error('An error occurred:', error.message);
+// }
+
+try {
+    // console.log(name);
+    console.log("i am here");
+    
+} catch (error) {
+    console.error('An error occurred:', error.message); 
+
+}
