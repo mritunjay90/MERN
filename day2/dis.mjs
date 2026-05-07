36d546caabc1246d14bdfb3be73d4cde877eb678
mritunjay90 <mrityunjayshah981427@gmail.com>
2026-05-07 11:25:50 +0545

    day2 first commit

---
+++ day2/dis.mjs
@@ -0,0 +1,11 @@
+// let [a , b ,c]=[10,11,12];
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//object destructuring
+
+let {name, weight, ismarried} = {name: "mrs", weight: 50, isMarried: false};
+console.log(name);
+console.log(weight);
+console.log(ismarried);
