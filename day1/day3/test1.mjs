24908e6c92f030bae2a22a8210a81fba897c1f86
mritunjay90 <mrityunjayshah981427@gmail.com>
2026-05-08 22:14:05 +0545

    cal

--- day1/day3/test1.mjs
+++ day1/day3/test1.mjs
@@ -17,3 +17,16 @@ rl.question("Enter expression: ", (input) => {
   }
   rl.close();
 });
+
+// add feature
+if (process.argv.includes('--version')) {
+  console.log("Simple Calculator version 1.0");
+  process.exit(0);
+}   
+if (process.argv.includes('--help')) {
+  console.log("Usage: node test.mjs [options]");
+  console.log("Options:");
+  console.log("  --version   Show version number");
+  console.log("  --help      Show this help message");
+  process.exit(0);
+}
