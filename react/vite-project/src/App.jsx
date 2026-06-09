aeba10090f3372afd3ae268d7d2352261ae66716
mritunjay90 <mrityunjayshah981427@gmail.com>
2026-06-09 12:32:31 +0545

    react

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+const App=()=>{
+  let name= "mrs"
+  let surname= "shah"
+  return (<div>
+    <h1>Details</h1>
+    <p>Welcome to my app!</p>
+    <h2>Features:</h2>
+    <p>Name: {name}</p>
+    <p>Surname: {surname}</p>
+    <p>1+1 is {1+1}</p>
+    <img src="iron.jpg"/>
+    <img src="rdj.jpg"/>
+  </div>)
+};
+export default App;
