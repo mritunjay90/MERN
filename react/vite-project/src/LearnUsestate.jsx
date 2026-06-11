936a636349d080a0f3d9de681acf2b4a3c76e0b7
mritunjay90 <mrityunjayshah981427@gmail.com>
2026-06-11 13:34:16 +0545

    last commit

--- react/vite-project/src/LearnUsestate.jsx
+++ react/vite-project/src/LearnUsestate.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const LearnUsestate = () => {
+  const [name, setName] = useState("mr shah")
+  const[count, setCount] = useState(0)
+
   return (
     <div>
+      <p>Name: {name}</p>
+      <p>Count: {count}</p>
+      <button onClick={()=> {
+        console.log("button clicked")
+      }}>click me</button>
+      <button onClick={()=> {
+        setName(" Shah")
+      }}>name change</button>
+      <button onClick={()=> {
+        setCount(count + 1)
+        console.log(count + 1)
+      }}>+</button>
+      <button onClick={()=> {
+        setCount(count - 1)
+        console.log(count - 1)
+      }}>-</button>
       
     </div>
-  )
-}
+  );
+};
 
 export default LearnUsestate
